05603d90c3839634f32b737462d1031d153f9636
capricorn <8250820+capricorn@users.noreply.github.com>
2023-08-25 14:29:52 -0600

    Create LocationDelegate skeleton

---
+++ mtnmap/LocationDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  LocationDelegate.swift
+//  mtnmap
+//
+//  Created by Collin Palmer on 8/25/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationDelegate: NSObject, CLLocationManagerDelegate {
+}
