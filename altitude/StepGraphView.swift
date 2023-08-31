be1d87737642ec9ccd1be9c5eec943a56632b2e0
capricorn <8250820+capricorn@users.noreply.github.com>
2023-08-31 14:53:02 -0600

    Draw timestamps beneath columns

--- altitude/StepGraphView.swift
+++ altitude/StepGraphView.swift
@@ -11,6 +11,31 @@ import WidgetKit
 struct StepGraphView: View {
     let columns = 5
     
+    private func graphHeight(context: GraphicsContext, size: CGSize) -> CGFloat {
+        let timestampText = Text("00:00").font(.system(size: 8, design: .monospaced))
+        
+        return size.height - (context.resolve(timestampText).measure(in: size).height + 4)
+    }
+    
+    private func drawTimeline(context: GraphicsContext, size: CGSize) {
+        let startDate = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm"
+        
+        for i in 0..<columns {
+            let timestamp = Int(startDate.timeIntervalSince1970) + i*Int.random(in: 60...3600)
+            let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+            let text = Text(formatter.string(from: date)).font(.system(size: 6, design: .monospaced))
+            
+            let columnWidth = size.width/CGFloat(columns)
+            let timestampTextSize = context.resolve(text).measure(in: size)
+            let timestampPadding = (columnWidth - timestampTextSize.width)/2
+            let labelPoint = CGPoint(x: CGFloat(i)*columnWidth + timestampPadding, y: size.height - timestampTextSize.height)
+            
+            context.draw(text, at: labelPoint, anchor: .topLeading)
+        }
+    }
+    
     var body: some View {
         ZStack {
             AccessoryWidgetBackground()
@@ -20,6 +45,7 @@ struct StepGraphView: View {
                     Path { path in
                         let values = (0..<columns).map { _ in CGFloat(Int.random(in: 100...1000)) }
                         let columnWidth = size.width/CGFloat(columns)
+                        let graphHeight = graphHeight(context: context, size: size)
                         
                         let scaler = { (x: CGFloat) -> CGFloat in
                             let maxValue = values.max()!
@@ -33,7 +59,7 @@ struct StepGraphView: View {
                         
                         for col in 0..<columns {
                             // +- 4 padding
-                            var columnY = (size.height-8)*scaler(values[col]) + 4
+                            var columnY = (graphHeight-8)*scaler(values[col]) + 4
                             path.addLine(to: CGPoint(x: CGFloat(col)*columnWidth, y: columnY))
                             
                             let point = CGPoint(
@@ -48,7 +74,7 @@ struct StepGraphView: View {
                             let columnTextPadding = (columnWidth - columnTextSize.width)/2
                             
                             // If the text will clip below the graph, render it above.
-                            if (columnY + columnTextSize.height) > (size.height-4) {
+                            if (columnY + columnTextSize.height) > (graphHeight-4) {
                                 columnY -= columnTextSize.height
                             }
                             
@@ -61,6 +87,8 @@ struct StepGraphView: View {
                         }
                     }, with: .foreground
                 )
+                
+                drawTimeline(context: context, size: size)
             }
             .border(Color.black)
         }
