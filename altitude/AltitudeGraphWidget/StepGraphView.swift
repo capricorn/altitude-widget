879ac244d6c3667cc35751badc1e30b329d5c960
capricorn <8250820+capricorn@users.noreply.github.com>
2023-09-08 11:39:30 -0600

    Use inverse rotation to draw text at 45 deg

--- altitude/AltitudeGraphWidget/StepGraphView.swift
+++ altitude/AltitudeGraphWidget/StepGraphView.swift
@@ -48,14 +48,10 @@ struct StepGraphView: View {
         let formatter = DateFormatter()
         formatter.dateFormat = "h:mm"
         
-        //context.rotate(by: .degrees(5))
         for i in 0..<min(StepGraphView.columns, entry.altitudes.count) {
             //let timestamp = Int(startDate.timeIntervalSince1970) + i*Int.random(in: 60...3600)
             let date = entry.altitudes[i].time
             let text = Text(formatter.string(from: date)).font(.system(size: 6, design: .monospaced))
-            
-            //context.rotate(by: .degrees(-90))
-            //context.transform = .identity
             
             let columnWidth = size.width/CGFloat(StepGraphView.columns)
             let timestampTextSize = context.resolve(text).measure(in: size)
@@ -63,36 +59,15 @@ struct StepGraphView: View {
             let x1 = (CGFloat(i)*columnWidth + columnWidth/2)// + timestampPadding
             // If not vertically centered, translation problem
             let y1 = 0.0//size.height//size.height-(timestampTextSize.width/2)///2//-20
-            let labelPoint = CGPoint(x: x1, y: y1)
             
-            // TODO: Calculate changed drawing rect once performed at 45 degrees..?
-            // (For now, don't use the text size at all for operations)
             context.drawLayer { subCtx in
-                //subCtx.translateBy(x: 0, y: 0)
-                // Does the point need translated too?
-                let rotation = 45.0
-                // Want to rotate opp direction to cancel out
-                let rotationRadians = Angle(degrees: -rotation).radians
-                subCtx.rotate(by: .degrees(rotation))
-                let hyp = labelPoint.distance()
+                subCtx.rotate(by: .degrees(45))
+                let inverseTransform = subCtx.transform.inverted()
+                let rotatedPoint = CGPoint(x: x1, y: y1).applying(inverseTransform)
                 
-                //let rotatedPoint = CGPoint(x: hyp*cos(rotationRadians), y: hyp*sin(rotationRadians))
-                let rotatedPoint = CGPoint(
-                    x: hyp*(cos(rotationRadians)-1)+x1,
-                    y: hyp*sin(rotationRadians)+y1
-                )
                 subCtx.draw(text, at: rotatedPoint, anchor: .topLeading)
-                //subCtx.draw(text, at: labelPoint.rotate(.degrees(-rotation)), anchor: .topLeading)
-                //subCtx.rotate(by: .degrees(-10))
-                //subCtx.transform = .init(rotationAngle: 3.14/4)
             }
-            //context.draw(text, at: labelPoint, anchor: .topLeading)
-            //context.transform = .identity
-            //context.rotate(by: .degrees(-5))
-            // TODO: Necessary?
-            //context.rotate(by: .degrees(-45))
         }
-        //context.transform = .identity
     }
     
     var body: some View {
@@ -130,12 +105,14 @@ struct StepGraphView: View {
                             let columnTextSize = context.resolve(columnText).measure(in: size)
                             let columnTextPadding = columnTextSize.width/2//(columnWidth - columnTextSize.width)/2
                             
+                            /*
                             context.draw(
                                 // TODO: Value formatting
                                 columnText,
                                 at: CGPoint(x: columnX - columnTextPadding, y: columnTopPoint.y - columnTextSize.height - 4),
                                 anchor: .topLeading
                             )
+                             */
                         }
                     }, with: .foreground, style: StrokeStyle(lineWidth: 4, lineCap: .round)
                 )
