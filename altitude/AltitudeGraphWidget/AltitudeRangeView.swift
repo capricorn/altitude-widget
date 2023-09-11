efadf9bd3fb747dc244708d49d90f37889f8fbc7
capricorn <8250820+capricorn@users.noreply.github.com>
2023-09-11 15:11:53 -0600

    Move range view drawing to draw method in vm

--- altitude/AltitudeGraphWidget/AltitudeRangeView.swift
+++ altitude/AltitudeGraphWidget/AltitudeRangeView.swift
@@ -13,64 +13,10 @@ import SwiftUI
 struct AltitudeRangeView: View {
     @ObservedObject var viewModel: AltitudeRangeViewModel
     
-    func verticalPadding(_ size: CGSize) -> CGFloat {
-        return size.height / 4
-    }
-    
-    func horizontalPadding(_ size: CGSize) -> CGFloat {
-        return size.width / 4
-    }
-    
     var body: some View {
         Canvas { context, size in
-            let centerX = size.width/2
-            let minX = horizontalPadding(size)
-            let maxX = size.width - horizontalPadding(size)
-            let minY = verticalPadding(size)
-            let maxY = size.height - verticalPadding(size)
-            
-            let path = Path { path in
-                let maxText = Text(viewModel.maxLabel).font(.system(size: 16).monospaced())
-                let maxTextSize = context.resolve(maxText).measure(in: size)
-                let topTextPoint = CGPoint(x: (minX + (maxX-minX)/2) - maxTextSize.width/2, y: minY-maxTextSize.height-8)
-                
-                // Top line text
-                context.draw(Text(viewModel.maxLabel), at: topTextPoint, anchor: .topLeading)
-                
-                let minText = Text(viewModel.minLabel).font(.system(size: 16).monospaced())
-                let minTextSize = context.resolve(minText).measure(in: size)
-                let bottomTextPoint = CGPoint(x: (minX + (maxX-minX)/2) - minTextSize.width/2, y: maxY+8)
-                
-                // Bottom line text
-                context.draw(Text(viewModel.minLabel), at: bottomTextPoint, anchor: .topLeading)
-                
-                // Top bar
-                path.move(to: CGPoint(x: minX, y: minY))
-                path.addLine(to: CGPoint(x: maxX, y: minY))
-                
-                path.move(to: CGPoint(x: centerX, y: minY))
-                path.addLine(to: CGPoint(x: centerX, y: maxY))
-                
-                path.move(to: CGPoint(x: minX, y: maxY))
-                path.addLine(to: CGPoint(x: maxX, y: maxY))
-                
-                guard let current = viewModel.current,
-                      let max = viewModel.max,
-                      let min = viewModel.min
-                else {
-                    return
-                }
-                
-                // Calculate y-offset given point (between maxY/minY)
-                let scalar = (1/(Swift.max(max-min,1.0)))*(current-max) + 1.0
-                let currentY = scalar * (maxY-minY) + minY
-                let centerPoint = CGPoint(x: centerX, y: currentY)
-                path.move(to: centerPoint)
-                path.addArc(center: centerPoint, radius: 4.0, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
-            }
-            
-            // TODO: Some sort of indicator to represent current point
-            context.stroke(path, with: .color(.white), style: StrokeStyle(lineWidth: 4, lineCap: .round))
+            let frame = CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height)
+            viewModel.drawRangeDisplay(context: &context, rect: frame)
         }
         .border(Color.red)
         .background(.black)
--- altitude/AltitudeGraphWidget/AltitudeRangeViewModel.swift
+++ altitude/AltitudeGraphWidget/AltitudeRangeViewModel.swift
@@ -30,4 +30,65 @@ class AltitudeRangeViewModel: ObservableObject {
         
         return Double(min).formatted(.number.precision(.fractionLength(0..<2)))
     }
+    
+    func verticalPadding(_ size: CGSize) -> CGFloat {
+        return size.height / 4
+    }
+    
+    func horizontalPadding(_ size: CGSize) -> CGFloat {
+        return size.width / 4
+    }
+    
+    func drawRangeDisplay(context: inout GraphicsContext, rect: CGRect) {
+                let size = rect.size
+        
+        let centerX = size.width/2
+        let minX = horizontalPadding(size)
+        let maxX = size.width - horizontalPadding(size)
+        let minY = verticalPadding(size)
+        let maxY = size.height - verticalPadding(size)
+        
+        let path = Path { path in
+            let maxText = Text(maxLabel).font(.system(size: 16).monospaced())
+            let maxTextSize = context.resolve(maxText).measure(in: size)
+            let topTextPoint = CGPoint(x: (minX + (maxX-minX)/2) - maxTextSize.width/2, y: minY-maxTextSize.height-8)
+            
+            // Top line text
+            context.draw(Text(maxLabel), at: topTextPoint, anchor: .topLeading)
+            
+            let minText = Text(minLabel).font(.system(size: 16).monospaced())
+            let minTextSize = context.resolve(minText).measure(in: size)
+            let bottomTextPoint = CGPoint(x: (minX + (maxX-minX)/2) - minTextSize.width/2, y: maxY+8)
+            
+            // Bottom line text
+            context.draw(Text(minLabel), at: bottomTextPoint, anchor: .topLeading)
+            
+            // Top bar
+            path.move(to: CGPoint(x: minX, y: minY))
+            path.addLine(to: CGPoint(x: maxX, y: minY))
+            
+            path.move(to: CGPoint(x: centerX, y: minY))
+            path.addLine(to: CGPoint(x: centerX, y: maxY))
+            
+            path.move(to: CGPoint(x: minX, y: maxY))
+            path.addLine(to: CGPoint(x: maxX, y: maxY))
+            
+            guard let current = current,
+                  let max = max,
+                  let min = min
+            else {
+                return
+            }
+            
+            // Calculate y-offset given point (between maxY/minY)
+            let scalar = (1/(Swift.max(max-min,1.0)))*(current-max) + 1.0
+            let currentY = scalar * (maxY-minY) + minY
+            let centerPoint = CGPoint(x: centerX, y: currentY)
+            path.move(to: centerPoint)
+            path.addArc(center: centerPoint, radius: 4.0, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+        }
+        
+        // TODO: Some sort of indicator to represent current point
+        context.stroke(path, with: .color(.white), style: StrokeStyle(lineWidth: 4, lineCap: .round))
+    }
 }
