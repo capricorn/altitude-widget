950df6f4a854903381f850d53c1d666279ce55e2
capricorn <8250820+capricorn@users.noreply.github.com>
2023-10-31 15:40:40 -0600

    Move user defaults to SettingsStorage

---
+++ altitude/Settings/SettingsStorage.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsStorage.swift
+//  mtnmap
+//
+//  Created by Collin Palmer on 10/31/23.
+//
+
+import Foundation
+
+extension UserDefaults {
+    enum Settings {
+        enum AltitudeUnit: String, CaseIterable, Identifiable {
+            case feet
+            case meters
+            
+            var label: String {
+                switch self {
+                case .feet: "Feet"
+                case .meters: "Meters"
+                }
+            }
+            
+            var id: Self {
+                self
+            }
+            
+            static let defaultKey = "altitude_unit"
+        }
+        
+        enum TimeNotation: String, CaseIterable, Identifiable {
+            case hour12
+            case hour24
+            
+            var label: String {
+                switch self {
+                case .hour12:
+                    "24-hour"
+                case .hour24:
+                    "12-hour"
+                }
+            }
+            
+            var id: Self {
+                self
+            }
+            
+            static let defaultKey = "time_notation"
+        }
+    }
+}
--- altitude/Settings/SettingsView.swift
+++ altitude/Settings/SettingsView.swift
@@ -8,45 +8,9 @@
 import SwiftUI
 
 struct SettingsView: View {
-    // TODO: Move elsewhere?
-    enum AltitudeUnit: String, CaseIterable, Identifiable {
-        case feet
-        case meters
-        
-        var label: String {
-            switch self {
-            case .feet: "Feet"
-            case .meters: "Meters"
-            }
-        }
-        
-        var id: Self {
-            self
-        }
-        
-        static let defaultKey = "altitude_unit"
-    }
-    
-    enum TimeNotation: String, CaseIterable, Identifiable {
-        case hour12
-        case hour24
-        
-        var label: String {
-            switch self {
-            case .hour12:
-                "24-hour"
-            case .hour24:
-                "12-hour"
-            }
-        }
-        
-        var id: Self {
-            self
-        }
-        
-        static let defaultKey = "time_notation"
-    }
-    
+    typealias AltitudeUnit = UserDefaults.Settings.AltitudeUnit
+    typealias TimeNotation = UserDefaults.Settings.TimeNotation
+
     @AppStorage(AltitudeUnit.defaultKey) private var unitSelection: AltitudeUnit?
     @AppStorage(TimeNotation.defaultKey) private var timeSelection: TimeNotation?
     
